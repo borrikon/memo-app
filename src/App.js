f5159bfd9b8dc0f78c44741048fe1d7e9374c10f
borrikon <borrikon@gmail.com>
2022-03-28 19:37:15 +0300

    add mui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import MainPage from './components/mainPage/mainPage';
 
 function App() {
   return (
     <div className="App">
+      <MainPage />
     </div>
   );
 }
---
+++ src/components/mainPage/mainPage.js
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+import styles from './styles.css';
+
+function MainPage(){
+    return(
+        <div className={styles.greeting}>
+            <Button>Hello</Button>
+        </div>
+    )
+}
+
+export default MainPage;
